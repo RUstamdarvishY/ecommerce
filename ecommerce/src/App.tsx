7b2cd1e17f32122acc90953d79c5de219c4089d1
rustam <darvishi123@yandex.ru>
2023-05-10 19:23:26 +0300

    init commit, first layout scheme

---
+++ ecommerce/src/App.tsx
@@ -0,0 +1,39 @@
+import { Grid, GridItem } from "@chakra-ui/react";
+import "./App.css";
+import NavBar from "./components/Header/NavBar";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        lg: `"header header"
+                  "panel panel"
+                  "nav main"
+                  "footer footer"`,
+      }}
+      gridTemplateRows={"1fr 3fr 5fr 1fr"}
+      gridTemplateColumns={"25% 75%"}
+      gap="3"
+      color="blackAlpha.700"
+      fontWeight="bold"
+    >
+      <GridItem pl="2" area={"header"}>
+        <NavBar/>
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"panel"}>
+        Panel
+      </GridItem>
+      <GridItem pl="2" bg="pink.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <GridItem pl="2" bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+      <GridItem pl="2" bg="blue.300" area={"footer"}>
+        Footer
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
